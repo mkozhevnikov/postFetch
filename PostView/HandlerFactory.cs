fe4ac4c4ce29234acc28128ce46109010ecbe54c
Михаил Кожевников < >
2014-06-06 16:43:45 +0600

    using HandlerFactory to invoke handler presentation layer shouldn't know about handler factory shouldn't know about model

---
+++ PostView/HandlerFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PostFetcher.Handler;
+
+namespace PostView {
+    internal class HandlerFactory {
+        /// <summary>
+        /// Вызывает обработку веб страницы
+        /// </summary>
+        /// <typeparam name="T">Тип обработчика</typeparam>
+        /// <typeparam name="TM">Тип обрабатываемой модели</typeparam>
+        /// <param name="firstPage">Стартовая страница для обработки</param>
+        public static void InvokeHandlerProcess<T, TM>(string firstPage) where T : IHandler<TM>, new() {
+            new T().Process(firstPage);
+        }
+
+        /// <summary>
+        /// Вызывает обработку веб страницы
+        /// </summary>
+        /// <typeparam name="TM">Тип обрабатываемой модели</typeparam>
+        /// <param name="handlerType">Тип обработчика</param>
+        /// <param name="firstPage">Стартовая страница для обработки</param>
+        public static void Call<TM>(Type handlerType, string firstPage) {
+            var info = typeof (HandlerFactory).GetMethod("InvokeHandlerProcess");
+            info.MakeGenericMethod(handlerType, typeof (TM)).Invoke(null, new[] {firstPage});
+        }
+
+        /// <summary>
+        /// Создает набор типов, которые реализуют интерфейс IHandler
+        /// </summary>
+        /// <typeparam name="TM">Тип обрабатываемой модели</typeparam>
+        public static IEnumerable<Type> GetHandlerTypes<TM>() {
+            var type = typeof (IHandler<TM>);
+            return AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(s => s.GetTypes())
+                .Where(p => type.IsAssignableFrom(p) && !p.IsAbstract && !p.IsInterface);
+        }
+    }
+}
--- PostView/LoadDialog.cs
+++ PostView/LoadDialog.cs
@@ -1,48 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Configuration;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Windows.Forms;
-using PostFetcher;
-using PostFetcher.Handler;
 using PostFetcher.Model;
 
 namespace PostView
 {
-    public partial class LoadDialog : Form {
+    public partial class LoadDialog : Form
+    {
         private ViewForm parent;
         public LoadDialog(ViewForm viewForm)
         {
             InitializeComponent();
             parent = viewForm;
-            
-            //составляю выборка из классов обработчиков
-            var type = typeof(IHandler<Article>);
-            var types = AppDomain.CurrentDomain.GetAssemblies()
-                                 .SelectMany(s => s.GetTypes())
-                                 .Where(p => type.IsAssignableFrom(p) && !p.IsAbstract && !p.IsInterface);
+
+            //составляем выборку из классов обработчиков
+            var types = HandlerFactory.GetHandlerTypes<Article>();
 //            handler.Items.AddRange(types.Select(t => (object)t.Name).ToArray());
             handler.Items.AddRange(types.Select(t => (object)t).ToArray());
         }
 
-        private void button1_Click(object sender, EventArgs e) {
-            try {
-                var type = handler.SelectedItem as Type;
-                if (type != null) {
-                    //создаю экземпляр класса обработчика
-                    var parser = (IHandler<Article>) Activator.CreateInstance(type);
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var handlerType = handler.SelectedItem as Type;
+                if (handlerType != null)
+                {
+//                    var parser = (IHandler<Article>) Activator.CreateInstance(type);
 //                    var parser = (IHandler<Article>) type.GetConstructor(new Type[] { }).Invoke(new object[] { });
-                    parser.Process(firstPage.Text);
+//                    parser.Process(firstPage.Text);
+                    //создаем экземпляр класса обработчика и вызываем его метод
+                    HandlerFactory.Call<Article>(handlerType, firstPage.Text);
                     MessageBox.Show("Новости загружены");
                     Close();
                     parent.RefreshGrid();
                 }
             }
-            catch (Exception ex) {
+            catch (Exception ex)
+            {
                 MessageBox.Show(ex.Message);
             }
         }
